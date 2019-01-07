076fe5071dcada79e256debf5df923f78f89dc2e
hisagames <hisagames@users.noreply.github.com>
2019-01-07 22:37:38 +0700

    Task 36 (100%)

---
+++ Pempek Story/Assets/Game Assets/Scripts/Base Script/PopupNote.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopupNote : MonoBehaviour
+{
+    enum PopupNoteType
+    {
+        BottomInfo
+    };
+
+    [SerializeField]
+    string popupNoteString;
+
+    [SerializeField]
+    PopupNoteType popupNoteType;
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            switch (popupNoteType)
+            {
+                case PopupNoteType.BottomInfo:
+                    PopupNoteManager.instance.BottomInfo.SetActive(true);
+                    PopupNoteManager.instance.UpdateBottomInfoText(popupNoteString);
+                    break;
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            switch (popupNoteType)
+            {
+                case PopupNoteType.BottomInfo:
+                    PopupNoteManager.instance.BottomInfo.SetActive(false);
+                    break;
+
+            }
+        }
+    }
+}
---
+++ Pempek Story/Assets/Game Assets/Scripts/Base Script/PopupNoteManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopupNoteManager : MonoBehaviour
+{
+    public static PopupNoteManager instance;
+
+    public GameObject BottomInfo;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        BottomInfo.SetActive(false);
+    }
+
+    public void UpdateBottomInfoText(string text)
+    {
+        BottomInfo.GetComponentInChildren<Text>().text = text;
+    }
+}
